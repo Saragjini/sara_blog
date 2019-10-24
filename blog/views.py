532e14c35c2d24559b07fc0e0947cafdbc87a30d
greentickss <sara.osmanagaj@gmail.com>
2019-10-24 20:25:53 +0200

    Added template and view for post detail

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render   #render ben kalimin e kodit html ne python
+from django.shortcuts import render,get_object_or_404  #render ben ekstraktimin e te dhenave te db dhe html e shfaq ne python
+from .models import Post
 
-# Create your views here.
+# Create your views here.  
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.order_by('-created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) #funksioni perdoret per te kapur nje rresht nga db
+    return render(request, 'blog/post_detail.html', {'post': post})
